ee99d3cc57e350fbc4c0d4b50ce5080027c0f9f4
diwakarg123 <Diwakar.G@thomsonreuters.com>
2019-07-18 15:39:43 +0530

    test

--- src/pmd/MyClass.java
+++ src/pmd/MyClass.java
@@ -1,6 +1,9 @@
 package pmd;
 //comments added more
 public class MyClass {
-	  public static void main(String[] args) {
+	public static void main(String[] args) {
+	    int x = 100 + 50;
+	    //System.out.println(x);
 	  }
-	}
+	
+}
