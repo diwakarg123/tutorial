a69c303e59b421c2f3d100cb7cd01bcb5153156c
diwakarg123 <Diwakar.G@thomsonreuters.com>
2019-07-18 15:33:37 +0530

    done

--- src/pmd/MyClass.java
+++ src/pmd/MyClass.java
@@ -3,9 +3,5 @@ package pmd;
 public class MyClass {
 	  public static void main(String[] args) {
 	    int x = 100 + 50;
-	    System.out.println(x);
-	  }
-	  public static void subfunction(){
-		  int y = 10 +50;
 	  }
 	}
