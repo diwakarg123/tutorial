e10725846e0825447a0aecc63159418ff2b46a7d
diwakarg123 <Diwakar.G@thomsonreuters.com>
2019-07-22 09:48:34 +0530

    update

--- src/pmd/MyClass.java
+++ src/pmd/MyClass.java
@@ -2,6 +2,14 @@ package pmd;
 //comments added more
 public class MyClass {
 	public static void main(String[] args) {
-
+		
+		try {
+			int[] myNumbers = {1, 2, 3};
+			system.out.println(myNumbers[10]);
+		} 
+		catch (Exception e) {
+			System.out.println("Something went wrong.");
+		}
+		
 	  }
 }
