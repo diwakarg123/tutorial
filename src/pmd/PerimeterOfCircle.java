77e89f26b18d409dbdbd766389f56ae1b6792bf7
diwakarg123 <Diwakar.G@thomsonreuters.com>
2019-07-25 14:35:09 +0530

    message

---
+++ src/pmd/PerimeterOfCircle.java
@@ -0,0 +1,22 @@
+package pmd;
+
+public class PerimeterOfCircle {
+
+	 public static void main(String args[]) 
+	    {   
+
+	           
+		   double r= 3.5;
+	         
+		   PerimeterOfCircle.area(r);
+	 
+	      }
+	     public static double area(double r)
+	    {   
+	       
+		   double  a=(22*2*r)/7;
+	 
+	           return a;
+	      }
+	
+}
