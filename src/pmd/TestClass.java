d219b56234efaae992c66b0f2fc1c511631de81c
diwakarg123 <Diwakar.G@thomsonreuters.com>
2020-03-09 14:53:02 +0530

    create class

---
+++ src/pmd/TestClass.java
@@ -0,0 +1,27 @@
+package pmd;
+//comment added
+public class MyClass {
+
+	public void main(String args[]) 
+    {   
+
+           
+	   double r= 3.5;
+	  area(r);
+	  try {
+		wait(1200);
+	  }
+	  catch(Exception e) {
+		  
+	  }
+ 
+      }
+     public static double area(double r)
+    {   
+       
+	   double  a=(22*6*r)/7;
+	   			
+           return a;
+      }
+	
+}
