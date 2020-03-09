21ed11abcc59df6ac4391b2f8361752641a2a113
diwakarg123 <Diwakar.G@thomsonreuters.com>
2020-03-09 16:24:48 +0530

    update

---
+++ src/pmd/NewClass.java
@@ -0,0 +1,24 @@
+package pmd;
+
+public class NewClass {
+
+		
+		public void main(String args[]) 
+	    {   
+
+	           
+		   double r= 3.5;
+		   area(r);
+
+	      }
+	     public static double area(double r)
+	    {   
+	       
+		   double  a=(22*33*r)/7;
+		   			
+	           return a;
+	      }
+
+
+	
+}
